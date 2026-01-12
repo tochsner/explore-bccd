b7ddaa3b09b862371dac862dd376ecfa4f8505dc
Tobia Ochsner <tobia.ochsner@hotmail.com>
2026-01-12 14:25:58 +0100

    Add example.

--- src/lib/components/FileUploadSection.svelte
+++ src/lib/components/FileUploadSection.svelte
@@ -39,9 +39,32 @@
 			}
 		}
 	}
+
+	async function loadExample() {
+		isLoadingPosterior = true;
+		try {
+			const content = await (await fetch('/iskandari_beast_run2_(preview).trees')).text();
+
+			await new Promise<void>((resolve, reject) => {
+				const handler = (e: MessageEvent<TreeWorkerResponse>) => {
+					if (e.data.success) {
+						posteriorTreesLoaded = true;
+						resolve();
+					} else {
+						reject(new Error(e.data.error));
+					}
+					worker.removeEventListener('message', handler);
+				};
+				worker.addEventListener('message', handler);
+				worker.postMessage({ type: 'parsePosteriorTrees', content });
+			});
+		} finally {
+			isLoadingPosterior = false;
+		}
+	}
 </script>
 
-<div class="flex items-stretch justify-between gap-12">
+<div class="flex flex-col items-center justify-between gap-6">
 	<label
 		style="background: linear-gradient(45deg, hsla(324, 35%, 40%) 0%, hsla(324, 30%, 45%) 100%)"
 		class="flex h-[250px] w-[400px] cursor-pointer flex-col items-center justify-center gap-8 rounded-2xl border border-white/70 p-14 text-white shadow-lg shadow-gray-400/10 transition hover:scale-105"
@@ -65,7 +88,7 @@
 				<path d="M7 9l5 -5l5 5" />
 				<path d="M12 4l0 12" />
 			</svg>
-			<span class="mb-2 text-center text-lg">1. Upload your posterior trees (.trees)</span>
+			<span class="mb-2 text-center text-lg">Upload your posterior trees (.trees)</span>
 		{:else}
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -93,4 +116,35 @@
 			class="file:bg-accent-light file:text-accent pointer-events-none mt-2 block w-full file:cursor-pointer file:rounded-md file:border-0 file:px-4 file:py-2 file:font-semibold"
 		/>
 	</label>
+
+	<span>or</span>
+
+	<button
+		style="background: linear-gradient(45deg, hsla(324, 0%, 50%) 0%, hsla(324, 0%, 55%) 100%)"
+		class="flex h-[30px] w-[400px] cursor-pointer flex-col items-center justify-center gap-8 rounded-2xl border border-white/70 p-10 text-white shadow-lg shadow-gray-400/10 transition hover:scale-105"
+		onclick={loadExample}
+	>
+		{#if isLoadingPosterior}
+			<Spinner />
+			<span class="mb-2 text-center text-lg">Loading and parsing trees...</span>
+		{:else if !posteriorTreesLoaded}
+			<span class="text-center text-lg">Try out an example</span>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="size-20 text-green-700"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+					d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
+				/><path d="M9 12l2 2l4 -4" /></svg
+			>
+
+			<span class="mb-2 text-center text-lg">Posterior trees loaded</span>
+		{/if}
+	</button>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,16 @@
 </svelte:head>
 
 <div class="bg-background text-dark flex h-lvh w-lvw flex-col items-center font-sans">
-	<header class="bg-accent/10 z-10 flex w-full rounded-b-2xl px-8 py-4">
+	<header class="bg-accent/10 z-10 flex w-full justify-between rounded-b-2xl px-8 py-4">
 		<span class="text-[1.8rem] font-bold">Explore<span class="text-accent">BCCD</span></span>
+
+		<a
+			class="cursor-pointer rounded-lg border border-white bg-white/20 px-4 py-2 transition hover:bg-white/50"
+			href="/"
+			data-sveltekit-reload
+		>
+			Explore a different dataset
+		</a>
 	</header>
 
 	{@render children()}
